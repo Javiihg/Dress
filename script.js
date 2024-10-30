2d421faf8eac59fa5210fff2fab5f7367a8f3263
Javi <hjavi127@gmail.com>
2024-10-30 10:24:17 +0000

    Correcciones

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const blob = document.querySelector('.blob');
 
-document.addEventListener('mousemove', (event) => {
-    const { clientX, clientY } = event;
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
+// Movimiento del blob según la posición del ratón
+if (blob) {
+    document.addEventListener('mousemove', (event) => {
+        const { clientX, clientY } = event;
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
 
-    const moveX = (clientX - centerX) * 0.05;
-    const moveY = (clientY - centerY) * 0.05;
+        const moveX = (clientX - centerX) * 0.05;
+        const moveY = (clientY - centerY) * 0.05;
 
-    blob.style.transform = `translate(${moveX}px, ${moveY}px)`;
-});
+        blob.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    });
+}
